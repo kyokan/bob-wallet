6cdeb8fe93b7464775790ae1fbbcdae1c8ce1605
Chi Kei Chan <chikeichan@gmail.com>
2020-10-12 20:18:41 -0700

    frontend perf improvement: pagination, initialization, polling

--- app/ducks/myDomains.js
+++ app/ducks/myDomains.js
@@ -2,43 +2,52 @@ import walletClient from '../utils/walletClient';
 
 const FETCH_MY_NAMES_START = 'app/myDomains/fetchMyNamesStart';
 const FETCH_MY_NAMES_STOP = 'app/myDomains/fetchMyNamesStop';
+const ADD_NAME = 'app/myDomains/addName';
+const ADD_NAMES = 'app/myDomains/addNames';
 
 const initialState = {
-  names: [],
+  names: {},
   isFetching: false,
   errorMessage: '',
 };
 
 export const getMyNames = () => async (dispatch, getState) => {
   const {
-    wallet: {address},
+    myDomains: {
+      names,
+      isFetching,
+    },
   } = getState();
 
-  dispatch({type: FETCH_MY_NAMES_START});
+  if (isFetching) return;
+
+  dispatch({ type: FETCH_MY_NAMES_START });
 
   try {
     const result = await walletClient.getNames();
-    const ret = [];
+    let ret = {};
+    let len = 0;
 
     for (let i = 0; i < result.length; i++) {
       const domain = result[i];
-      const {owner} = domain;
-      const coin = await walletClient.getCoin(owner.hash, owner.index);
+      const {owner, name} = domain;
 
-      if (coin && domain.state === 'CLOSED') {
-        ret.push(domain);
-        dispatch({
-          type: FETCH_MY_NAMES_STOP,
-          payload: ret.slice(),
-        });
+      if (!names[name]) {
+        const coin = await walletClient.getCoin(owner.hash, owner.index);
+        if (coin) {
+          ret[name] = domain;
+          len++;
+        }
       }
     }
 
-    // console.log(ret);
-    // dispatch({
-    //   type: FETCH_MY_NAMES_STOP,
-    //   payload: ret,
-    // });
+    dispatch({
+      type: ADD_NAMES,
+      payload: ret,
+    });
+
+    dispatch({ type: FETCH_MY_NAMES_STOP });
+
   } catch (error) {
     dispatch({
       type: FETCH_MY_NAMES_STOP,
@@ -54,11 +63,39 @@ export default function myDomainsReducer(state = initialState, action) {
   const {type, payload, error} = action;
   switch (type) {
     case FETCH_MY_NAMES_START:
-      return {...state, isFetching: true};
+      return {
+        ...state,
+        isFetching: true,
+        errorMessage: '',
+      };
     case FETCH_MY_NAMES_STOP:
       return error
-        ? {...state, isFetching: false, errorMessage: error.message}
-        : {...state, isFetching: false, names: payload};
+        ? {
+          ...state,
+          isFetching: false,
+          errorMessage: error.message,
+        }
+        : {
+          ...state,
+          isFetching: false,
+          errorMessage: '',
+        };
+    case ADD_NAMES:
+      return {
+        ...state,
+        names: {
+          ...state.names,
+          ...payload,
+        },
+      };
+    case ADD_NAME:
+      return {
+        ...state,
+        names: {
+          ...state.names,
+          [payload.name]: payload,
+        },
+      };
     default:
       return state;
   }
