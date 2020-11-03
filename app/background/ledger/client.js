a4ac9e98bd73edcd44264f7394ec5ea56c7f0b06
Matthew Slipper <me@matthewslipper.com>
2020-11-03 01:20:52 -0800

    Cleanup

--- app/background/ledger/client.js
+++ app/background/ledger/client.js
@@ -1,6 +1,5 @@
 import { makeClient } from '../ipc/ipc';
 
 export const clientStub = (ipcRendererInjector) => makeClient(ipcRendererInjector, 'Ledger', [
-  'getXPub',
-  'signTransaction'
+  'getXPub'
 ]);
