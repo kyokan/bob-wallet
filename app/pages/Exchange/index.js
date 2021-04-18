42fd4e9c15afd53400338dabfdf7f32338d3a6cb
kurumiimari <78248217+kurumiimari@users.noreply.github.com>
2021-04-18 07:49:21 -0600

    Add ShakeDex fee support (#327)
    * Add ShakeDex fee support
    
    * add fee address to confirm modal
    
    * remove fees from table view
    
    Co-authored-by: Chi Kei Chan <chikeichan@gmail.com>

--- app/pages/Exchange/index.js
+++ app/pages/Exchange/index.js
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 import moment from 'moment';
 import classNames from 'classnames';
 const {dialog} = require('electron').remote;
-
+import { shell } from 'electron';
 import { clientStub as aClientStub } from '../../background/analytics/client.js';
 import { clientStub as sClientStub } from '../../background/shakedex/client.js';
 import { HeaderItem, HeaderRow, Table, TableItem, TableRow } from '../../components/Table';
@@ -38,6 +38,7 @@ import GenerateListingModal from "./GenerateListingModal";
 import {getPageIndices} from "../../utils/pageable";
 import Dropdown from "../../components/Dropdown";
 import SpinnerSVG from '../../assets/images/brick-loader.svg';
+import ConfirmFeeModal from './ConfirmFeeModal.js';
 
 const analytics = aClientStub(() => require('electron').ipcRenderer);
 const shakedex = sClientStub(() => require('electron').ipcRenderer);
@@ -56,6 +57,8 @@ class Exchange extends Component {
       isPlacingListing: false,
       isUploadingFile: false,
       isGeneratingListing: false,
+      isShowingFeeConfirmationFor: false,
+      feeInfo: null,
       generatingListing: null,
       isLoading: true,
     };
@@ -190,6 +193,19 @@ class Exchange extends Component {
         throw e;
       }, 0);
     }
+  };
+
+  onClickSubmitShakedex = async (listing) => {
+    const feeInfo = await shakedex.getFeeInfo();
+
+    if (feeInfo.rate === 0) {
+      return this.props.submitToShakedex(listing.auction);
+    }
+
+    this.setState({
+      isShowingFeeConfirmationFor: listing,
+      feeInfo,
+    });
   };
 
   renderListingStatus(status) {
@@ -428,6 +444,16 @@ class Exchange extends Component {
             })}
           />
         )}
+        {this.state.isShowingFeeConfirmationFor && (
+          <ConfirmFeeModal
+            listing={this.state.isShowingFeeConfirmationFor}
+            feeInfo={this.state.feeInfo}
+            onClose={() => this.setState({
+              isShowingFeeConfirmationFor: null,
+              feeInfo: null,
+            })}
+          />
+        )}
       </div>
     );
   }
@@ -573,7 +599,7 @@ class Exchange extends Component {
               </div>
               <div
                 className="bid-action__link"
-                onClick={() => this.props.submitToShakedex(l.auction)}
+                onClick={() => this.onClickSubmitShakedex(l)}
               >
                 Submit
               </div>
@@ -595,10 +621,13 @@ class Exchange extends Component {
     const currentBid = getCurrentBid(auction);
 
     return (
-      <TableRow key={auction.id}>
+      <TableRow
+        key={auction.id}
+        className="exchange__auction-listing__row"
+        onClick={() => shell.openExternal(`https://shakedex.com/a/${auction.name}`)}
+      >
         <TableItem>{formatName(auction.name)}</TableItem>
         <TableItem>{displayBalance(currentBid?.price, true)}</TableItem>
-        <TableItem>{displayBalance(auction.bids[0].price, true)}</TableItem>
         <TableItem>{this.renderNextBid(auction)}</TableItem>
         <TableItem>
           {
@@ -606,16 +635,22 @@ class Exchange extends Component {
               <div className="exchange__auction-row-buttons">
                 <div
                   className="bid-action__link"
-                  onClick={() => this.setState({
-                    placingAuction: auction,
-                    placingCurrentBid: currentBid,
-                  })}
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    this.setState({
+                      placingAuction: auction,
+                      placingCurrentBid: currentBid,
+                    });
+                  }}
                 >
                   Fill
                 </div>
                 <div
                   className="bid-action__link"
-                  onClick={() => this.onClickDownload(auction)}
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    this.onClickDownload(auction);
+                  }}
                 >
                   Download Proofs
                 </div>
@@ -660,7 +695,6 @@ function Header() {
     <HeaderRow>
       <HeaderItem>Name</HeaderItem>
       <HeaderItem>Current Bid</HeaderItem>
-      <HeaderItem>Starting Bid</HeaderItem>
       <HeaderItem>Next Bid</HeaderItem>
       <HeaderItem />
     </HeaderRow>
