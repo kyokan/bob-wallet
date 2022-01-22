ef9206a5741788f9ecac308e8bf78b3b24052003
faltrum <70612135+faltrum@users.noreply.github.com>
2022-01-23 00:04:35 +0100

    Update i18n.js
    Updating Spanish and CAtalan LAnguages

--- app/utils/i18n.js
+++ app/utils/i18n.js
@@ -17,6 +17,8 @@ export default translations;
 export const languageDropdownItems = [
   { label: 'English (US)', value: 'en-US' },
   // { label: '中文', value: 'zh' },
+  { label: 'Español (ES)', value: 'es-ES' },
+  { label: 'Català (CAT)', value: 'ca' },
   { label: 'Custom JSON', value: 'custom' },
 ];
 
