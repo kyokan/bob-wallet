8ff6a152125c929faa5b4cd6bf051c945118a2c5
Rithvik Vibhu <rithvikvibhu@gmail.com>
2022-02-12 00:20:23 +0530

    sentry: load sentry and app based on process

--- app/sentry.js
+++ app/sentry.js
@@ -1,17 +1,20 @@
-const Sentry = (
-  process.type === 'renderer'
-  ? require('@sentry/electron/renderer')
-  : require('@sentry/electron/main')
-);
+let Sentry, app;
 
-const electron = require('electron');
+if (process.type === 'renderer') {
+  Sentry = require('@sentry/electron/renderer');
+  app = require('@electron/remote').app;
+} else {
+  Sentry = require('@sentry/electron/main');
+  app = require('electron').app;
+}
+
 const pkg = require('../package.json');
 
 (function () {
-  // Will not initialize in renderer process, for now
-  if (!electron.app || !electron.app.isPackaged) {
+  if (!app.isPackaged) {
     return;
   }
+
   Sentry.init({
     dsn: 'https://ea41895688674e598d69cbd975872db8@sentry.io/1759225',
     release: 'bob-wallet@' + pkg.version,
