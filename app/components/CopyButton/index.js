ee8abdb533fbb2ca355f2bc59ffeb7624ff16eda
Rithvik Vibhu <rithvikvibhu@gmail.com>
2022-10-07 23:03:14 +0530

    ui: change copy button text on copy

--- app/components/CopyButton/index.js
+++ app/components/CopyButton/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import c from 'classnames';
 import copy from 'copy-to-clipboard';
 import PropTypes from 'prop-types';
+import { I18nContext } from "../../utils/i18n";
 import './copy-btn.scss';
 
 export default class CopyButton extends Component {
@@ -14,17 +15,30 @@ export default class CopyButton extends Component {
     className: '',
   };
 
+  static contextType = I18nContext;
+
   state = {
     hasCopied: false,
   };
 
+  timer = null;
+
+  componentWillUnmount() {
+    if (this.timer) clearTimeout(this.timer);
+  }
+
   copyAddress = () => {
     copy(this.props.content);
     this.setState({ hasCopied: true });
-    setTimeout(() => this.setState({ hasCopied: false }), 2500);
+    this.timer = setTimeout(() => {
+      this.setState({ hasCopied: false });
+      this.timer = null;
+    }, 2500);
   };
 
   render() {
+    const {t} = this.context;
+
     return (
       <button
         className={c('copy-btn', this.props.className, {
@@ -32,7 +46,7 @@ export default class CopyButton extends Component {
         })}
         onClick={this.copyAddress}
       >
-        Copy
+        {this.state.hasCopied ? `${t('copied')}!` : t('copy')}
       </button>
     )
   }
