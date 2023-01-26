868bd728475ca530159702ea6eae89ec74255e5a
miguelgargallo <andorra.mg@gmail.com>
2023-01-26 09:35:49 +0100

    Ledger Fix for Arch Linux

---
+++ tools/ledger-fix.sh
@@ -0,0 +1,12 @@
+##
+# Ledger fix for Arch Linux users.
+# execute this on the root of bob-wallet directory.
+#
+# 2023 © Miguel Gargallo and Rithvik Vibhu
+#
+# Install the rules for the Ledger and npm install the hsd-ledger package on bob-wallet by Kyokan.
+#
+# First run: chmod +x ./ledger-fix.sh
+# then run: ./ledger-fix.sh
+##
+wget -q -O - https://raw.githubusercontent.com/LedgerHQ/udev-rules/master/add_udev_rules.sh | sudo bash && npm install github:handshake-org/hsd-ledger#v2.0.2
